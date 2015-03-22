cb3f9bd3ab574bda5b103143be6c43604fd050ae
Anton Verinov <git@zemlan.in>
2015-03-22 11:46:55 +0200

    2 space indention

--- src/main.js
+++ src/main.js
@@ -7,146 +7,146 @@ var componentsRegistry = {};
 var wrappersRegistry = {};
 
 if (!Function.prototype.bind) {
-    // Credits to https://github.com/kdimatteo/bind-polyfill
-    // Solves https://github.com/ariya/phantomjs/issues/10522
-    /* eslint-disable no-extend-native */
-    Function.prototype.bind = function (oThis) {
-        if (typeof this !== "function") {
-            // closest thing possible to the ECMAScript 5 internal IsCallable function
-            throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-        }
+  // Credits to https://github.com/kdimatteo/bind-polyfill
+  // Solves https://github.com/ariya/phantomjs/issues/10522
+  /* eslint-disable no-extend-native */
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
 
-        var aArgs = Array.prototype.slice.call(arguments, 1),
-            fToBind = this,
-            fNOP = function () {},
-            fBound = function () {
-                return fToBind.apply(
-                    this instanceof fNOP && oThis ? this : oThis,
-                    aArgs.concat(Array.prototype.slice.call(arguments))
-                );
-            };
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+      fToBind = this,
+      fNOP = function () {},
+      fBound = function () {
+        return fToBind.apply(
+          this instanceof fNOP && oThis ? this : oThis,
+          aArgs.concat(Array.prototype.slice.call(arguments))
+        );
+      };
 
-        fNOP.prototype = this.prototype;
-        fBound.prototype = new fNOP();
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
 
-        return fBound;
-    };
-    /* eslint-enable no-extend-native */
+    return fBound;
+  };
+  /* eslint-enable no-extend-native */
 }
 
 function _getOrRequireComponent(name) {
-    if (componentsRegistry[name] === void 0) {
-        componentsRegistry[name] = require(name);
-    }
+  if (componentsRegistry[name] === void 0) {
+    componentsRegistry[name] = require(name);
+  }
 
-    return componentsRegistry[name];
+  return componentsRegistry[name];
 }
 
 function _bindComponentToNode(wrappedNode, componentName) {
-    var bazId = wrappedNode.id;
+  var bazId = wrappedNode.id;
 
-    if (nodesComponentsRegistry[bazId] === void 0) {
-        nodesComponentsRegistry[bazId] = [];
+  if (nodesComponentsRegistry[bazId] === void 0) {
+    nodesComponentsRegistry[bazId] = [];
+  }
+
+  if (nodesComponentsRegistry[bazId].indexOf(componentName) === -1) {
+    nodesComponentsRegistry[bazId].push(componentName);
+    var component = _getOrRequireComponent(componentName);
+    var bazFunc;
+
+    if (component.f) {
+      bazFunc = component.f;
+    } else {
+      bazFunc = component;
     }
 
-    if (nodesComponentsRegistry[bazId].indexOf(componentName) === -1) {
-        nodesComponentsRegistry[bazId].push(componentName);
-        var component = _getOrRequireComponent(componentName);
-        var bazFunc;
-
-        if (component.f) {
-            bazFunc = component.f;
-        } else {
-            bazFunc = component;
-        }
-
-        if (component.deps && component.deps.length) {
-            Array.prototype.forEach.call(
-                component.deps,
-                _bindComponentToNode.bind(null, wrappedNode)
-            );
-        }
-
-        bazFunc(wrappedNode.__wrapped__);
+    if (component.deps && component.deps.length) {
+      Array.prototype.forEach.call(
+        component.deps,
+        _bindComponentToNode.bind(null, wrappedNode)
+      );
     }
+
+    bazFunc(wrappedNode.__wrapped__);
+  }
 }
 
 function BazookaWrapper(node) {
-    var bazId = node.getAttribute('data-bazid');
+  var bazId = node.getAttribute('data-bazid');
 
-    if (bazId === void 0 || bazId === null) {
-        bazId = _bazId++;
-        node.setAttribute('data-bazid', bazId);
-        wrappersRegistry[bazId] = this;
-    }
+  if (bazId === void 0 || bazId === null) {
+    bazId = _bazId++;
+    node.setAttribute('data-bazid', bazId);
+    wrappersRegistry[bazId] = this;
+  }
 
-    this.__wrapped__ = node;
-    this.id = parseInt(bazId, 10);
+  this.__wrapped__ = node;
+  this.id = parseInt(bazId, 10);
 }
 
 BazookaWrapper.prototype.constructor = BazookaWrapper;
 
 function _wrapAndBindNode(node) {
-    var componentName = node.getAttribute('data-bazooka');
-    var wrappedNode = new BazookaWrapper(node);
+  var componentName = node.getAttribute('data-bazooka');
+  var wrappedNode = new BazookaWrapper(node);
 
-    _bindComponentToNode(wrappedNode, componentName);
+  _bindComponentToNode(wrappedNode, componentName);
 }
 
 function registerMethod(bazId, methodName, method) {
-    if (methodsRegistry[bazId] === void 0) {
-        methodsRegistry[bazId] = {};
-    }
+  if (methodsRegistry[bazId] === void 0) {
+    methodsRegistry[bazId] = {};
+  }
 
-    methodsRegistry[bazId][methodName] = method;
+  methodsRegistry[bazId][methodName] = method;
 }
 
 function getMethod(bazId, methodName) {
-    return methodsRegistry[bazId][methodName];
+  return methodsRegistry[bazId][methodName];
 }
 
 BazookaWrapper.prototype.g = function (methodName) {
-    return getMethod(this.bazId, methodName);
+  return getMethod(this.bazId, methodName);
 };
 BazookaWrapper.prototype.r = function (methodName, method) {
-    registerMethod(this.bazId, methodName, method);
+  registerMethod(this.bazId, methodName, method);
 };
 
 var Bazooka = function (value) {
-    if (value instanceof BazookaWrapper) {
-      return value;
-    }
+  if (value instanceof BazookaWrapper) {
+    return value;
+  }
 
-    return new BazookaWrapper(value);
+  return new BazookaWrapper(value);
 };
 
 Bazooka.wrapper = BazookaWrapper;
 Bazooka.parseNodes = function () {
-    Array.prototype.forEach.call(
-        document.querySelectorAll("[data-bazooka]"),
-        _wrapAndBindNode
-    );
+  Array.prototype.forEach.call(
+    document.querySelectorAll("[data-bazooka]"),
+    _wrapAndBindNode
+  );
 };
 Bazooka.refresh = function () {
-    for (var bazId in wrappersRegistry) {
-        if (wrappersRegistry[bazId] && !wrappersRegistry[bazId].__wrapped__.parentNode) {
-            wrappersRegistry[bazId] = null;
-            nodesComponentsRegistry[bazId] = [];
-            methodsRegistry[bazId] = {};
-        }
+  for (var bazId in wrappersRegistry) {
+    if (wrappersRegistry[bazId] && !wrappersRegistry[bazId].__wrapped__.parentNode) {
+      wrappersRegistry[bazId] = null;
+      nodesComponentsRegistry[bazId] = [];
+      methodsRegistry[bazId] = {};
     }
-    Array.prototype.forEach.call(
-        document.querySelectorAll("[data-bazooka]:not([data-bazid])"),
-        _wrapAndBindNode
-    );
+  }
+  Array.prototype.forEach.call(
+    document.querySelectorAll("[data-bazooka]:not([data-bazid])"),
+    _wrapAndBindNode
+  );
 };
 Bazooka.watch = function () {
-    var i = setInterval(
-        Bazooka.refresh,
-        200
-    );
+  var i = setInterval(
+    Bazooka.refresh,
+    200
+  );
 
-    return clearInterval.bind(null, i);
+  return clearInterval.bind(null, i);
 };
 
 module.exports = Bazooka;
