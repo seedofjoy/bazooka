962f36c36fdfca82b326300dcab08b8e3a80c716
Anton Verinov <git@zemlan.in>
2015-03-28 15:29:06 +0200

    helpers/getAttrSpec
    more tests and getAttrs refactoring

--- src/helpers.js
+++ src/helpers.js
@@ -1,46 +1,54 @@
 'use strict';
 
-var rbrace = /^(?:\{[\w\W]*\}|\[[\w\W]*\])$/;
+var IGNORED_ATTRS = ['data-bazid', 'data-bazooka'];
+
+var rbrace = /^(?:\{.*\}|\[.*\])$/;
 var rdataAttr = /^data-(.+)$/;
 var rdashAlpha = /-([\da-z])/gi;
 var fcamelCase = function (all, letter) {
   return letter.toUpperCase();
 };
 
-var getAttrs = function (node) {
-  var ignoredAttrs = ['data-bazid', 'data-bazooka'];
-  var parsedAttrs = {};
+function _parseAttr(parsedAttrs, attr) {
+  if (typeof attr.value !== 'string') { return parsedAttrs; }
 
-  Array.prototype.forEach.call(node.attributes, function (attr) {
-    if ( !(rdataAttr.test(attr.name) && ignoredAttrs.indexOf(attr.name) === -1) ) {
-        return;
-    }
+  if ( !rdataAttr.test(attr.name) || IGNORED_ATTRS.indexOf(attr.name) !== -1) {
+      return parsedAttrs;
+  }
 
-    var data = attr.value;
-    var attrName = attr.name.match(rdataAttr)[1];
-    var camelCaseName = attrName.replace(rdashAlpha, fcamelCase);
+  var attrName = attr.name.match(rdataAttr)[1];
+  var camelCaseName = attrName.replace(rdashAlpha, fcamelCase);
 
-    if (typeof data === 'string') {
+  var data;
+
+  switch (attr.value) {
+    case 'true':
+      data = true;
+      break;
+    case 'false':
+      data = false;
+      break;
+    case 'null':
+      data = null;
+      break;
+    default:
       try {
-        if (data === 'true') {
-          data = true;
-        } else if (data === 'false') {
-          data = false;
-        } else if (data === 'null') {
-          data = null;
-        } else if (data === +data + '') {
-          data = +data;
-        } else if (rbrace.test(data)) {
-          data = JSON.parse(data);
+        if (attr.value === +attr.value + '') {
+          data = +attr.value;
+        } else if (rbrace.test(attr.value)) {
+          data = JSON.parse(attr.value);
+        } else {
+          data = attr.value;
         }
-      } catch (e) {}
-    } else {
-      data = undefined;
-    }
-    parsedAttrs[camelCaseName] = data;
-  });
+      } catch (e) { return parsedAttrs; }
+  }
 
+  parsedAttrs[camelCaseName] = data;
   return parsedAttrs;
+}
+
+var getAttrs = function (node) {
+  return Array.prototype.reduce.call(node.attributes, _parseAttr, {});
 };
 
 module.exports = {
