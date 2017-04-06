77295a310863c74227a9e8fc26ca7b6ef3ade519
Anton Verinov <git@zemlan.in>
2017-04-06 15:03:19 +0300

    lint

--- examples/_hot/component.js
+++ examples/_hot/component.js
@@ -16,13 +16,11 @@ function clickHandler(node, state) {
 }
 
 export default function hotComponent(node) {
-  const state = module.hot
-    ? Baz(node).HMRState(module.hot, model())
-    : model();
+  const state = module.hot ? Baz(node).HMRState(module.hot, model()) : model();
 
   if (module.hot) {
     // reload page if `./model.js` is changed
-    module.hot.decline('./model.js')
+    module.hot.decline('./model.js');
   }
 
   mockRerender(node, state);
